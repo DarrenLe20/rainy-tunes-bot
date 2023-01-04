549899818f512a09d5d65f34eb84b34d5c784a0a
DarrenLe20 <89809192+DarrenLe20@users.noreply.github.com>
2023-01-03 21:14:05 -0500

    removed test weather to save API calls

--- test_bot.py
+++ test_bot.py
@@ -22,23 +22,6 @@ class TestBot(unittest.TestCase):
         self.assertIsNotNone(main.twitter_auth())
         self.assertIsNotNone(main.spotify_auth())
 
-    def test_get_weather_fail(self):
-        self.assertEqual(main.get_weather("aaaaaaa"),
-                         ("Error: Could not get weather data", 0))
-
-    def test_get_weather_success(self):
-        WEATHERSTACK_API_KEY = os.getenv("WEATHERSTACK_API_KEY")
-        req = requests.get(
-            "http://api.weatherstack.com/current?access_key=" + WEATHERSTACK_API_KEY + "&query=New York")
-        response = req.json()
-        description = response['current']['weather_descriptions'][0]
-        weather_code = response['current']['weather_code']
-        time = response['current']['observation_time']
-        msg = "New York (" + main.get_current_date() + \
-            " - " + time + "): " + description
-        self.assertEqual(main.get_weather(), (msg, weather_code))
-        self.assertIn(weather_code, constants.WEATHER_CODES.keys())
-
     def test_get_recommendations(self):
         sp = main.spotify_auth()
         genre = random.choice(sp.recommendation_genre_seeds())
@@ -54,9 +37,3 @@ class TestBot(unittest.TestCase):
         self.assertIsNotNone(main.tracks_data["track_name"])
         self.assertIsNotNone(main.tracks_data["valence"])
         self.assertIsNotNone(main.tracks_data["url"])
-
-    def test_get_weather_cond(self):
-        msg, weather_code = main.get_weather()
-        for key, val in constants.WEATHER_CATEGORIES.items():
-            if weather_code in val:
-                self.assertEqual(key, main.get_cond(weather_code))
